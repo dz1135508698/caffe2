a6839d687a08ea394ad9d649e95cab291f2cfef9
Yangqing Jia <me@daggerfs.com>
2017-10-02 10:54:54 -0700

    temp change for cmake and conda

--- conda/build.sh
+++ conda/build.sh
@@ -6,6 +6,9 @@ if [ -z "$PREFIX" ]; then
   PREFIX="$CONDA_PREFIX"
 fi
 
+# Point CMake to use conda specific paths.
+export CMAKE_LIBRARY_PATH=$PREFIX/lib:$PREFIX/include:$CMAKE_LIBRARY_PATH
+
 # conda build will copy everything over, including build directories.
 # Don't let this pollute hte build!
 rm -rf build || true
@@ -14,8 +17,18 @@ PYTHON_ARGS="$(python ./scripts/get_python_cmake_flags.py)"
 
 mkdir -p build
 cd build
-cmake -DBLAS=MKL -DMKL_INCLUDE_DIR=$CONDA_PREFIX/include -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_PREFIX_PATH="$PREFIX" $CONDA_CMAKE_ARGS $PYTHON_ARGS ..
-make -j20
+# TODO(Yangqing): turn on rocksdb when available.
+cmake \
+    -DBLAS=MKL \
+    -DMKL_INCLUDE_DIR=$CONDA_PREFIX/include \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    -DCMAKE_PREFIX_PATH="$PREFIX" \
+    -DUSE_ROCKSDB=OFF \
+    $CONDA_CMAKE_ARGS \
+    $PYTHON_ARGS \
+    ..
+# which cmake && exit 1
+make -j$(nproc)
 
 make install/fast
 
